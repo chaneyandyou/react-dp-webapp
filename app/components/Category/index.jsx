6cfa07b6cf2457bcafc346f76d523a2d8d51cc22
chaneychan <chaneychan@126.com>
2017-06-15 00:06:07 +0800

    add:react-swipe

--- app/components/Category/index.jsx
+++ app/components/Category/index.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import PureRenderMixin from 'react-addons-pure-render-mixin'
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import ReactSwipe from 'react-swipe';
 import './style.less'
 
 class Category extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+            categoryIndex:0
+        }
     }
+
     render() {
+        let options = {
+            auto: 2000,
+            callback: (index)=>{
+                this.setState({categoryIndex:index});
+            }
+        };
         return (
-            <div>Category</div>
+            <div>
+                <ReactSwipe className="carousel" swipeOptions={options}>
+                    <div>PANE 1</div>
+                    <div>PANE 2</div>
+                    <div>PANE 3</div>
+                </ReactSwipe>
+                {this.state.categoryIndex}
+            </div>
         )
     }
 }
